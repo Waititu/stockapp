8baf211cb4132bd95fc442329e83d04aee674706
Isaac Waititu <waitituisaac@gmail.com>
2020-03-21 14:47:50 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebar MiddleWare
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "This is other stuff!";
+
+//Set Handlebar routes
+app.get('/', function (req, res) {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+// Set static folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server listening on Port:' + PORT));
+
