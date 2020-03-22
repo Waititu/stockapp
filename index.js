7be4160376c797c91aad70ee282b8122cf440fbb
Isaac Waititu <waitituisaac@gmail.com>
2020-03-21 19:43:20 -0700

    created Procfile for heroku

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const request = require('request');
+const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 5000;
+
+// Use body parser middleware 
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// API Key pk_3b428a3c8fc94424bf46830e540b0f86
+// Create Call API Function
+function call_api(finishedAPI, ticker){
+
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_3b428a3c8fc94424bf46830e540b0f86', {json: true}, (err, res, body) => {
+
+    if (err)  {return console.log(err);}
+    if(res.statusCode === 200){
+         //console.log(body);
+         finishedAPI(body);
+        };
+    });
+}
 
 // Set Handlebar MiddleWare
 app.engine('handlebars', exphbs());
@@ -14,11 +33,31 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "This is other stuff!";
 
-//Set Handlebar routes
+//Set Handlebar index GET routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: otherstuff
-    });
+    call_api(function(doneAPI){
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, "fb");
+});
+
+
+//Set Handlebar index POST routes
+
+app.post('/', function (req, res) {
+    call_api(function(doneAPI){
+        //posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker );
+});
+
+
+//Create about page route
+app.get('/aboutme.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folders
